904b11f6df3737cab9244436a85faa6ae8bc7996
Othereum <seokjin.dev@gmail.com>
2020-06-01 15:22:55 +0900

    feat: Add Alloc(), Free(), New(), Delete()

--- include/omem.hpp
+++ include/omem.hpp
@@ -34,6 +34,12 @@ namespace omem
 			return cnt + remain;
 		}
 
+		template <class T>
+		[[nodiscard]] constexpr T PadToPowerOf2(T x) noexcept
+		{
+			return size_t(1) << LogCeil(x, 2);
+		}
+		
 		template <class T1, class T2>
 		[[nodiscard]] constexpr auto Max(T1 a, T2 b) noexcept
 		{
@@ -70,14 +76,63 @@ namespace omem
 			}
 		};
 
-		template <size_t SizeLog2>
+		template <size_t Size>
 		struct MemoryPoolSelector
 		{
 			static MemoryPool& Get() noexcept
 			{
-				return MemoryPoolSelectorImpl<Max(1 << SizeLog2, sizeof(void*))>::Get();
+				return MemoryPoolSelectorImpl<Max(PadToPowerOf2(Size), sizeof(void*))>::Get();
 			}
 		};
+	}
+
+	/**
+	 * \brief Allocate Size bytes of memory from pool
+	 * \tparam Size Bytes of memory to be allocated
+	 * \return Allocated memory
+	 * \note If no memory left in the pool, allocate new memory
+	 * \note MUST BE RETURNED by Free<Size>(p) with SAME SIZE
+	 */
+	template <size_t Size>
+	[[nodiscard]] void* Alloc()
+	{
+		return detail::MemoryPoolSelector<Size>::Get().Alloc();
+	}
+
+	/**
+	 * \brief Return memory to pool
+	 * \tparam Size MUST BE SAME SIZE as allocated by Alloc()
+	 * \param p Memory to be returned
+	 */
+	template <size_t Size>
+	void Free(void* p) noexcept
+	{
+		detail::MemoryPoolSelector<Size>::Get().Free(p);
+	}
+
+	/**
+	 * \brief Allocate memory from pool and create object
+	 * \tparam T Object type to be created
+	 * \param args Arguments to be passed to constructor of T
+	 * \return Created object
+	 * \note MUST BE DELETED by Delete<T>(p) with SAME TYPE. DO NOT CAST POINTER BEFORE DELETE
+	 */
+	template <class T, class... Args>
+	[[nodiscard]] T* New(Args&&... args)
+	{
+		return new (Alloc<sizeof T>()) T{std::forward<Args>(args)...};
+	}
+
+	/**
+	 * \brief Destroy object pointed by p and return memory to pool
+	 * \tparam T MUST BE SAME TYPE as allocated by New().
+	 * \param p Object to be deleted
+	 */
+	template <class T>
+	void Delete(T* p) noexcept
+	{
+		p->~T();
+		Free<sizeof T>(p);
 	}
 	
 	template <class T>
@@ -108,14 +163,24 @@ namespace omem
 	private:
 		[[nodiscard]] static auto& GetPool() noexcept
 		{
-			return detail::MemoryPoolSelector<detail::LogCeil(sizeof T, 2)>::Get();
+			return detail::MemoryPoolSelector<sizeof T>::Get();
+		}
+	};
+
+	template <class T>
+	class Deleter
+	{
+	public:
+		void operator()(T* p) const noexcept
+		{
+			Delete(p);
 		}
 	};
 
 	/**
 	 * \brief Register function to be called when memory pool is destroyed. Print info to stdout by default.
 	 * \param on_pool_dest function to be called
-	 * \note BE CAREFUL: CALLING THIS FUNCTION IS NOT THREAD-SAFE
+	 * \note CALLING THIS FUNCTION IS NOT THREAD-SAFE
 	 * \note All exceptions thrown from on_pool_dest are swallowed silently.
 	 */
 	void SetOnPoolDest(const std::function<void(const PoolInfo&)>& on_pool_dest);
@@ -123,7 +188,7 @@ namespace omem
 	/**
 	 * \brief Register function to be called when memory pool is destroyed. Print info to stdout by default.
 	 * \param on_pool_dest function to be called
-	 * \note BE CAREFUL: CALLING THIS FUNCTION IS NOT THREAD-SAFE
+	 * \note CALLING THIS FUNCTION IS NOT THREAD-SAFE
 	 * \note All exceptions thrown from on_pool_dest are swallowed silently.
 	 */
 	void SetOnPoolDest(std::function<void(const PoolInfo&)>&& on_pool_dest) noexcept;
